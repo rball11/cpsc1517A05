cd15dc93067821eadc2b4adde80e1b81e60cbf3f
rball11 <rball11@studentmail.nait.ca>
2019-01-11 16:13:27 -0700

    Update Die.cs

--- OOPsReview/OOPsReview/Die.cs
+++ OOPsReview/OOPsReview/Die.cs
@@ -16,6 +16,7 @@ namespace OOPsReview
             // private data members CANNOT be reached directly by the user
             // public data members CAN be reached directly by the user
         private int _Size;
+        private string _Color;
 
         //Properties
             //a property is an external interface between the user and a SINGLE piece of data within the instance
@@ -35,9 +36,47 @@ namespace OOPsReview
             {
                 //takes the supplied user value and places it into the internal private data member
                 //the incoming piece of data is placed into a special variable that is called: value
-                _Size = value;
+                //optionally you may place validation on the incoming value
+                if (value >= 6 && value <= 20)
+                {
+                    _Size = value;
+                }
+                else
+                {
+                    throw new Exception("Die cannot be " + value.ToString() + " sides. Die must have between 6 and 20 sides.");
+                }
             }
         }
+
+        public string Color
+        {
+            get
+            {
+                return _Color;
+            }
+            set
+            {
+                //if (value == null) will fail for an empty string
+                //(value == "") will fail for a null value
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Color has no value.");
+                }
+                else
+                {
+                    _Color = value;
+                }
+            }
+        }
+
+        //Auto Implemented Property 
+        //public
+        //it has a datatype, name
+        //DOES NOT HAVE an internal data member that you can DIRECTLY access
+        //the system will create, internally, a data storage area of the appropriate datatype and manage the area
+        //the only way to access the data of an auto-implemented property is via the property
+        //usually used when there is no need for any internal validationor other property logic
+        public int FaceValue { get; set; }
 
         //Constructors
 
