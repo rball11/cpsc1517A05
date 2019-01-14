30bd2b0d603c37a40064eedec59cc9dd815956ab
rball11 <rball11@studentmail.nait.ca>
2019-01-14 16:54:11 -0700

    Update Turn.cs

--- OOPsReview/OOPsReview/Turn.cs
+++ OOPsReview/OOPsReview/Turn.cs
@@ -8,6 +8,37 @@ namespace OOPsReview
 {
     class Turn
     {
+        private int _PlayerOneRoll;
 
+        public int PlayerOneRoll
+        {
+            get
+            {
+                return _PlayerOneRoll;
+            }
+            set
+            {
+                _PlayerOneRoll = value;
+            }
+        }
+
+        public int PlayerTwoRoll { get; set; }
+
+        public Turn()
+        {
+
+        }
+
+        public Turn(int playerOne, int playerTwo)
+        {
+            PlayerOneRoll = playerOne;
+            PlayerTwoRoll = playerTwo;
+        }
+
+        //methods
+        //public string FindRollResults()
+        //{
+        // return null;
+        //}
     }
 }
